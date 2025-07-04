2a1fe7564230d3096382a3859fcbc5e33d4308e6
chrisedeson <edesonchristopher@gmail.com>
2025-07-04 22:52:02 +0100

    work out solutions to group activity week 1

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,26 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        // Console.WriteLine(string.Join(", ", select));
+        List<int> results = new List<int>();
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                if (i < list1.Length)
+                {
+                    results.Add(list1[i]);
+                }
+            }
+            else
+            {
+                if (i < list2.Length)
+                {
+                    results.Add(list2[i]);
+                }
+
+            }
+        }
+        return results.ToArray();
     }
 }
